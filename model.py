1eb1d4011f378a344419bccc1546ec2997867b50
Merari Cisneros <meraridanielcisneros@gmail.com>
2021-10-16 10:02:35 -0700

    added model.py to modularize the creation of the model

---
+++ model.py
@@ -0,0 +1,18 @@
+'''
+Contains the functions for my predict applicant success model.
+To Do:
+
+1) Create a preprocessing function that acts on specified data file
+
+
+'''
+
+# Preprocessing function
+def PreProcess(filepath , dropCol , ):
+
+    '''(str) --> (Pandas Dataframe)
+    PreProcess takes a string filepath to a csv file and returns a cleaned pandas dataframe.
+     filepath (str): specifies path to csv file
+     dropCol (list of str): specifies columns to drop
+     
+       '''
