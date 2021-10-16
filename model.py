be04b254e46d120d673aedd3c5bce4b50421a20c
Merari Cisneros <meraridanielcisneros@gmail.com>
2021-10-16 15:18:26 -0700

    completed preprocessing function

--- model.py
+++ model.py
@@ -6,13 +6,62 @@ To Do:
 
 
 '''
+import pandas as pd
 
 # Preprocessing function
-def PreProcess(filepath , dropCol , ):
+def PreProcess(filepath , dropCols , thresholds ):
+
+
+
 
     '''(str) --> (Pandas Dataframe)
     PreProcess takes a string filepath to a csv file and returns a cleaned pandas dataframe.
      filepath (str): specifies path to csv file
      dropCol (list of str): specifies columns to drop
-     
+     thresholds (list of int)
        '''
+
+    # Read in the csv as dataframe
+    DF = pd.read_csv(filepath)
+
+    # Drop the columns specified in dropCol
+    DF.drop(dropCols , axis= 1, inplace= True)
+       
+   # Determine the number of unique values in each column
+    uValue_Counts = DF.nunique()
+
+   # Filter out columns with unique count greater than 10
+    greater_than_10 = uValue_Counts[uValue_Counts > 10]
+
+    # filter out columns that are not objects
+    categorical_cols = [col for col in greater_than_10.index if DF[col].dtypes == 'object']
+
+    # bin each col in categorical_cols according to their threshold
+    for i in range(0 , len(thresholds)):
+
+      type_counts = DF[categorical_cols[i]].value_counts()
+
+      less_than_thresh = type_counts[DF[categorical_cols[i]].value_counts() < thresholds[i]].index
+
+      DF[categorical_cols[i]] = DF[categorical_cols[i]].replace(less_than_thresh , 'other')
+
+    # convert categorical values to numeric
+    numeric_DF = pd.get_dummies(DF)
+
+    # Splitting target and inputs
+    y = numeric_DF.pop('IS_SUCCESSFUL') # Pop off "Is_Succesful" our target column
+    X = numeric_DF # the remaining features after the pop off
+    
+    return [y,X]
+
+
+
+
+
+
+
+
+
+ 
+
+
